5a4cfc581b3784af92982bb35d7852cfe8b04b0e
Mark Ferlatte <mark@truss.works>
2021-10-09 18:04:33 -0700

    Started to parse the CSV data.

--- main.py
+++ main.py
@@ -7,9 +7,39 @@ __author__ = "Mark Ferlatte <mark@truss.works>"
 __version__ = "0.0.1"
 __license__ = "Apache 2.0"
 
-def PaylocityCSVToData(csv_string):
-    return csv_string
+import csv
 
+def nameFromPaylocityName(p):
+    names = p.split(",")
+    return names[1].strip() + " " + names[0].strip()
+    
+def eventFromPaylocityCSVRow(row):
+    # row[6] is lastname, firstname
+    # row[8] is type
+    # row[9] is the start date
+    # row[10] is the end date
+    # row[13] is the status (Taken, Approved, Cancelled)
+    event = {
+        "name": nameFromPaylocityName(row[6]),
+        "type": row[8],
+        "start_date": row[9],
+        "end_date": row[10],
+        "status": row[13]
+    }
+    return event
+    
+def PaylocityCSVToData(csv_file):
+    csvreader = csv.reader(csv_file)
+    events = []
+    try:
+        for row in csvreader:
+            event = eventFromPaylocityCSVRow(row)
+            events.append(event)
+    except csv.Error as e:
+        print('line {}: {}'.format(csvreader.line_num, e))
+    print(events)
+    return "test"
+        
 def main():
     print(PaylocityCSVToData("Hello world"))
     
--- test_main.py
+++ test_main.py
@@ -4,7 +4,27 @@ import main
 class TestMain(unittest.TestCase):
 
     def test_PaylocityCSVToData(self):
-        csv_string = "TESTSTRING"
-        self.assertEqual(main.PaylocityCSVToData(csv_string), csv_string)
+        with open('test_data.csv', newline='') as csvfile:
+            data = main.PaylocityCSVToData(csvfile)
+        self.assertEqual(data, "test")
 
-        
+    def test_eventFromPaylocityCSVRow(self):
+        row = ["Company:  (123456)", "GL Categor: ", "Practice: ",
+               "Department: ", ": ", ": ", "Lastname, Firstname ", "000",
+               "Vacation", "01/01/2000", "01/02/2000", "8.00", "100.00",
+               "Taken", "Bosslast, Bossfirst"]
+        testEvent = {
+            "name": "Firstname Lastname",
+            "type": "Vacation",
+            "start_date": "01/01/2000",
+            "end_date": "01/02/2000",
+            "status": "Taken"
+        }
+        event = main.eventFromPaylocityCSVRow(row)
+        self.assertEqual(event, testEvent)
+
+    def test_nameFromPaylocityName(self):
+        testName = "Lastname, Firstname "
+        self.assertEqual(main.nameFromPaylocityName(testName),
+                         "Firstname Lastname")
+
